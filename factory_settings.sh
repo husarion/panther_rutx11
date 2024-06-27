5caf8f04d44ad5d42be1ef6ca37882ebae877733
Paweł pidpawel Kozubal <git@pidpawel.eu>
2024-06-27 13:54:16 +0200

    Apply Bash good practices to the factory reset script

--- factory_settings.sh
+++ factory_settings.sh
@@ -1,44 +1,25 @@
 #!/bin/bash
-
-#Script used for first setup of Teltonika RUTX11 
+set -euo pipefail
 
 rutx11_ip=192.168.1.1
 new_ip=10.15.20.1
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -a | --address )        shift
-                                rutx11_ip="$1"
-                                ;;
-        -n | --new-ip )         shift
-                                new_ip="$1"
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
-    esac
-    shift
-done
-#get serial number of Raspberry Pi
 rpi_serial=$(cat /proc/cpuinfo | awk '/Serial/ {print $3}')
-#get MAC of eth on Raspberry PI
 rpi_mac=$(ethtool --show-permaddr eth0 | awk '{print $3}')
-#get MAC of NUC
-ping husarion.lan -c 1
-nuc_mac=$(arp -a | awk '/husarion.lan/ {print $4}')
-	
-#create ssh private/public key pair without passphrase
-ssh-keygen -t rsa -b 4096 -q -N "" -f ~/.ssh/id_rsa
 
-#copy public key to RUTX11
-#human input of password is required
-ssh root@$rutx11_ip "tee -a /etc/dropbear/authorized_keys" < ~/.ssh/id_rsa.pub
+# create ssh private/public key pair without passphrase
+echo "USER: It's safe to say n in the next prompt"
+ssh-keygen -t rsa -b 4096 -q -N "" -f ~/.ssh/id_rsa || true
 
-#set config on RUTX11
-ssh root@$rutx11_ip << EOF
-    uci set dhcp.lan.time='12' 
+# copy public key to RUTX11
+# human input is required
+ssh-keygen -R $rutx11_ip
+echo "USER: Please say 'yes' and then enter current RUTX password"
+ssh root@$rutx11_ip "tee -a /etc/dropbear/authorized_keys" <~/.ssh/id_rsa.pub
+
+# set config on RUTX11
+ssh root@$rutx11_ip <<EOF
+    uci set dhcp.lan.time='12'
     uci set dhcp.lan.letter='h'
     uci set network.lan.ipaddr='10.15.20.1'
 
@@ -51,7 +32,7 @@ ssh root@$rutx11_ip << EOF
     uci set network.wwan.proto='dhcp'
     uci set firewall.@zone[1].network='wan wan6 mob1s1a1 mob1s2a1 wwan'
 
-    uci set ntpclient.@ntpclient[0].zoneName='Europe/Warsaw'  
+    uci set ntpclient.@ntpclient[0].zoneName='Europe/Warsaw'
     uci set system.system.timezone='CET-1CEST,M3.5.0,M10.5.0/3'
     uci set system.ntp.enabled='1'
     uci set ntpclient.@ntpclient[0].gps_sync='1'
@@ -63,7 +44,7 @@ ssh root@$rutx11_ip << EOF
     uci set gps.gpsd.galileo_sup='1'
     uci set gps.gpsd.glonass_sup='7'
     uci set gps.gpsd.beidou_sup='3'
-    
+
     uci set gps.nmea_forwarding.enabled='1'
     uci set gps.nmea_forwarding.hostname='10.15.20.2'
     uci set gps.nmea_forwarding.port='5000'
@@ -90,7 +71,7 @@ ssh root@$rutx11_ip << EOF
     uci set gps.PQGSV.forwarding_enabled='1'
     uci set gps.PQGSV.forwarding_interval='1'
     uci set gps.PQGSA.forwarding_enabled='1'
-    uci set gps.PQGSA.forwarding_interval='1'    
+    uci set gps.PQGSA.forwarding_interval='1'
 
     uci set wireless.radio0.channel='auto'
     uci set wireless.default_radio0.ssid='Panther_${rpi_serial: -4}'
@@ -114,19 +95,9 @@ ssh root@$rutx11_ip << EOF
     uci set dhcp.@host[-1].mac="$rpi_mac"
     uci set dhcp.@host[-1].name="rpi"
 
-    uci add dhcp host
-    uci set dhcp.@host[-1].ip="10.15.20.3"
-    uci set dhcp.@host[-1].mac="$nuc_mac"
-    uci set dhcp.@host[-1].name="nuc"
-
-
-
-    
     uci commit
-    reboot
-    
 EOF
 
+echo "USER: Config is now saved. Will reboot RUTX now. Feel free to close this session/terminal."
 
-
-
+ssh root@$rutx11_ip reboot || true
